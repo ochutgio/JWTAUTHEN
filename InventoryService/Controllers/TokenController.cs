d6f5179e8f819a7542d636fa35c86a481886045b
ochutgio <ochutgio@gmail.com>
2021-12-29 15:28:09 +0700

    init jwt authen

---
+++ InventoryService/Controllers/TokenController.cs
@@ -0,0 +1,138 @@
+﻿using InventoryService.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        public IConfiguration _configuration;
+        private readonly InventoryContext _context;
+
+        public TokenController(IConfiguration config, InventoryContext context)
+        {
+            _configuration = config;
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(UserInfo _userData)
+        {
+
+            if (_userData != null && _userData.Email != null && _userData.Password != null)
+            {
+                var user = await GetUser(_userData.Email, _userData.Password);
+
+                if (user != null)
+                {
+                    //create claims details based on the user information
+                    var claims = new[] {
+                    new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                    new Claim("Id", user.UserId.ToString()),
+                    new Claim("FirstName", user.FirstName),
+                    new Claim("LastName", user.LastName),
+                    new Claim("UserName", user.UserName),
+                    new Claim("Email", user.Email),
+                    new Claim("roles", "admin"),
+                    new Claim("roles", "user")
+                   };
+
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+                    var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                    var token = new JwtSecurityToken(
+                        _configuration["Jwt:Issuer"],
+                        _configuration["Jwt:Audience"],
+                        claims,
+                        expires: DateTime.UtcNow.AddDays(1),
+                        signingCredentials: signIn);
+
+                    var test = new JwtSecurityTokenHandler().WriteToken(token);
+
+                    return Ok(test);
+                }
+                else
+                {
+                    return BadRequest("Invalid credentials");
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("cookiejwt")]
+        public async Task<IActionResult> Cookie(UserInfo _userData)
+        {
+
+            if (_userData != null && _userData.Email != null && _userData.Password != null)
+            {
+                var user = await GetUser(_userData.Email, _userData.Password);
+
+                if (user != null)
+                {
+                    //create claims details based on the user information
+                    var claims = new[] {
+                    new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                    new Claim("Id", user.UserId.ToString()),
+                    new Claim("FirstName", user.FirstName),
+                    new Claim("LastName", user.LastName),
+                    new Claim("UserName", user.UserName),
+                    new Claim("Email", user.Email),
+                    new Claim("roles", "admin"),
+                    new Claim("roles", "user")
+                   };
+
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+                    var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                    var token = new JwtSecurityToken(
+                        _configuration["Jwt:Issuer"],
+                        _configuration["Jwt:Audience"],
+                        claims,
+                        expires: DateTime.UtcNow.AddDays(_configuration.GetValue<int>("Jwt:Expire")),
+                        signingCredentials: signIn);
+                    
+                    var test = new JwtSecurityTokenHandler().WriteToken(token);
+               
+                    HttpContext.Response.Cookies.Append(
+                        "X-Access-Token",
+                        test                     
+                    ) ;
+
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest("Invalid credentials");
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        private async Task<UserInfo> GetUser(string email, string password)
+        {
+            return await _context.UserInfo.FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
+        }
+    }
+}
---
+++ InventoryService/Startup.cs
@@ -0,0 +1,105 @@
+using InventoryService.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connection = Configuration.GetConnectionString("InventoryDatabase");
+            services.AddDbContextPool<InventoryContext>(options => options.UseSqlServer(connection));
+            services.AddControllers();
+
+            services.AddAuthentication(
+                        i =>
+                        {
+                            i.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                            i.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                            i.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                            i.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+                        }
+                    )
+                    .AddJwtBearer(options =>
+                    {
+                        options.RequireHttpsMetadata = true;
+                        options.SaveToken = true;
+
+                        options.TokenValidationParameters = new TokenValidationParameters()
+                        {
+                            ValidateIssuer = true,
+                            ValidateAudience = true,
+                            ValidAudience = Configuration["Jwt:Audience"],
+                            ValidIssuer = Configuration["Jwt:Issuer"],
+                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                        };
+
+                        options.Events = new JwtBearerEvents();
+                        options.Events.OnMessageReceived = context =>
+                        {
+                            if (context.Request.Cookies.ContainsKey("X-Access-Token"))
+                            {
+                                context.Token = context.Request.Cookies["X-Access-Token"];
+                            }
+                            return Task.CompletedTask;
+                        };
+                    })
+                    .AddCookie(options =>
+                    {
+                        options.Cookie.SameSite = SameSiteMode.Strict;
+                        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                        options.Cookie.IsEssential = true;
+                        options.Cookie.Expiration = System.TimeSpan.FromDays(Configuration.GetValue<int>("jwt:Expire"));
+                    });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            //app.UseCors(builder => 
+            //    builder
+            //    .AllowAnyHeader()
+            //    .AllowAnyMethod()
+            //    .AllowAnyOrigin()
+            //    //.AllowCredentials()
+            //);
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
